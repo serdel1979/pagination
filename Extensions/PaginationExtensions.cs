7fe81f44082cf3323b13d4f91848e24aeebae9c2
Sergio De Luca <sdlbsso@gmail.com>
2024-11-09 14:49:35 -0300

    Pagination created

---
+++ Extensions/PaginationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagination.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static async Task<PaginatedResult<T>> ToPaginatedListAsync<T>(
+        this IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+
+            return new PaginatedResult<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
