bf9ef01d0780710b13233861b6cdbdd3d9946331
Sergio De Luca <sdlbsso@gmail.com>
2024-11-09 14:49:35 -0300

    Pagination created

---
+++ PaginatedResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pagination
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Items { get; }
+        public int TotalCount { get; }
+        public int PageSize { get; }
+        public int PageNumber { get; }
+
+        public PaginatedResult(List<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            Items = items;
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
